41d259774c1336347504ff4da63bd72cac8416b1
ivan <ivanchen36@gmail.com>
2013-11-02 23:23:37 +0800

    subject:use the new style based on weibo content:delete user system and use weibo's

---
+++ themes/default/common/home_1.php
@@ -0,0 +1,30 @@
+<div class="category-hot-180-300">
+	<a target="_blank" href="<?php echo spUrl("detail","index", array("share_id"=> $category['s1']['share_id']));?>"><img src="<?php echo base_url($category['s1']['image_path'].'_home.jpg');?>" onerror="this.src='<?php echo base_url().'assets/img/blank.png'?>';"/></a>
+	<div class="pic-des">
+		<a href="<?php echo spUrl("detail","index", array("share_id"=> $category['s1']['share_id']));?>" target="_blank"><?php echo sysSubStr($category['s1']['intro'],15,true);?></a>
+	</div>
+</div>
+<div class="category-hot-180-300">
+	<a target="_blank" href="<?php echo spUrl("detail","index", array("share_id"=> $category['s2']['share_id']));?>"><img src="<?php echo base_url($category['s2']['image_path'].'_home.jpg');?>" onerror="this.src='<?php echo base_url().'assets/img/blank.png'?>';"/></a>
+	<div class="pic-des">
+		<a href="<?php echo spUrl("detail","index", array("share_id"=> $category['s2']['share_id']));?>" target="_blank"><?php echo sysSubStr($category['s2']['intro'],15,true);?></a>
+	</div>
+</div>
+<div class="category-hot-180-300">
+	<a target="_blank" href="<?php echo spUrl("detail","index", array("share_id"=> $category['s3']['share_id']));?>"><img src="<?php echo base_url($category['s3']['image_path'].'_home.jpg');?>" onerror="this.src='<?php echo base_url().'assets/img/blank.png'?>';"/></a>
+	<div class="pic-des">
+		<a href="<?php echo spUrl("detail","index", array("share_id"=> $category['s3']['share_id']));?>" target="_blank"><?php echo sysSubStr($category['s3']['intro'],15,true);?></a>
+	</div>
+</div>
+<div class="category-hot-350-145">
+	<a target="_blank" href="<?php echo spUrl("detail","index", array("share_id"=> $category['s4']['share_id']));?>"><img src="<?php echo base_url($category['s4']['image_path'].'_home.jpg');?>" onerror="this.src='<?php echo base_url().'assets/img/blank.png'?>';"/></a>
+	<div class="pic-des">
+		<a href="<?php echo spUrl("detail","index", array("share_id"=> $category['s4']['share_id']));?>" target="_blank"><?php echo sysSubStr($category['s4']['intro'],15,true);?></a>
+	</div>
+</div>
+<div class="category-hot-350-145">
+	<a target="_blank" href="<?php echo spUrl("detail","index", array("share_id"=> $category['s5']['share_id']));?>"><img src="<?php echo base_url($category['s5']['image_path'].'_home.jpg');?>" onerror="this.src='<?php echo base_url().'assets/img/blank.png'?>';"/></a>
+	<div class="pic-des">
+		<a href="<?php echo spUrl("detail","index", array("share_id"=> $category['s5']['share_id']));?>" target="_blank"><?php echo sysSubStr($category['s5']['intro'],15,true);?></a>
+	</div>
+</div>
